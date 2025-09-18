438bb0084205fe42218eda9ae7dfebf4c2c85b70
Zac Ginever <zgin653@UoA.auckland.ac.nz>
2025-09-19 10:39:24 +1200

    countdown timer

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import AnalogClock from './components/AnalogClock';
 function App() {
   const time = useDate()
   const [isDigital, setIsDigital] = useState(true)
+
+  console.log(((60 - 55) < 9) ? "0" + (60 - 55) : 60 - time.second)
   
   return (
     <>
@@ -18,18 +20,18 @@ function App() {
             <td>
               <table>
                 <tbody>
-                  <td className='time'>{time.hour[0]}</td>
-                  <td className='time-mono'>{time.hour[1]}</td>
+                  <td className='time'>{1 - time.hour[0]}</td>
+                  <td className='time-mono'>{6 - time.hour[1]}</td>
                   <td className='time'>:</td>
-                  <td className='time-mono'>{time.minute}</td>
+                  <td className='time-mono'>{((59 - time.minute) <= 9) ? "0" + (59 - time.minute) : 59 - time.minute}</td>
                   <td className='time'>:</td>
-                  <td className='time-mono'>{time.second}</td>
+                  <td className='time-mono'>{((59 - time.second) <= 9) ? "0" + (59 - time.second) : 59 - time.second}</td>
                 </tbody>
               </table>
             </td>
           </tr>
           <tr>
-            <td><p className='date'>{time.date}</p></td>
+            {/* <td><p className='date'>{time.date}</p></td> */}
           </tr>
         </tbody>
       </table> 
